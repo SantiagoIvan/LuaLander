b58642c1b2d7fd097b10f3aa64f94d86d2fde624
Santiago Feijoo <santiago.feijoo96@gmail.com>
2026-08-10 23:48:18 -0300

    el titulo de LowFuelUI cambia a OutOfFuel cuando en el baile te quedas sin nafta. Se reestablece cuando volver al estado LowFuel

--- Assets/Scripts/UI/LowFuelUI.cs
+++ Assets/Scripts/UI/LowFuelUI.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
 using System;
+using TMPro;
 
 public class LowFuelUI : MonoBehaviour
 {
     [SerializeField] private Animator animator;
+    [SerializeField] private TextMeshProUGUI lowFuelTextMeshPro;
     private string animatorWarningParameterName = "IsLowFuel";
+    
     private Lander lander;
 
     private void Start()
@@ -16,6 +19,7 @@ public class LowFuelUI : MonoBehaviour
     }
     private void Lander_OnLowFuel(object sender, EventArgs e)
     {
+        this.lowFuelTextMeshPro.text = "LOW FUEL";
         this.updateAnimator();
     }
     private void Lander_OnFuelCollected(object sender, OnFuelCollectedEventArgs e)
@@ -24,11 +28,10 @@ public class LowFuelUI : MonoBehaviour
     }
     private void Lander_OnOutOfFuel(object sender, EventArgs e)
     {
-
+        this.lowFuelTextMeshPro.text = "OUT OF FUEL";
     }
     private void updateAnimator()
     {
-        Debug.Log("Low fuel is "+ animatorWarningParameterName + " and its value is " + lander.isFuelLow());
         this.animator.SetBool(animatorWarningParameterName, lander.isFuelLow());
     }
 }
